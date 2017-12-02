91c2f26aa592b9c7bd3877174fb06845d2b0fead
liupj <MooN_2012@qq.com>
2017-12-02 23:44:04 +0800

    update

---
+++ src/main/java/win/elegentjs/designpattern/SingletonTest.java
@@ -0,0 +1,71 @@
+package win.elegentjs.designpattern;
+
+/**
+ * 设计模式：
+ *
+ *  Singleton
+ *
+ *  两种方式： 直接定义为静态成员变量
+ *            懒汉式
+ *
+ */
+public class SingletonTest {
+
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+
+        Singleton singleton1 = Singleton.getInstance();
+
+        System.out.println(singleton == singleton1);
+
+
+        Singleton2 singleton2 = Singleton2.getInstance();
+        Singleton2 singleton21 = Singleton2.getInstance();
+
+        System.out.println(singleton2 == singleton21);
+
+    }
+}
+
+
+
+class Singleton {
+
+    private static final Singleton singleton = new Singleton();
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
+
+
+class Singleton2 {
+
+    private static Singleton2 singleton2;
+
+    private Singleton2() {
+
+    }
+
+    public static Singleton2 getInstance() {
+        if (singleton2 == null) {
+            synchronized (Singleton2.class) {
+                if (singleton2 == null) {
+                    singleton2 = new Singleton2();
+                }
+            }
+
+        }
+
+        return singleton2;
+    }
+
+
+}
+
+
+
---
+++ src/main/java/win/elegentjs/obj/ObjectTest.java
@@ -0,0 +1,8 @@
+package win.elegentjs.obj;
+
+public class ObjectTest {
+
+    public static void main(String[] args) {
+
+    }
+}
