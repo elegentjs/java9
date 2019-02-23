cd3a28b843abe428efa18d438ae232e62ac73d3e
liupj <MooN_2012@qq.com>
2019-02-23 16:56:52 +0800

    update

---
+++ src/main/java/win/elegentjs/container/ArraysSample.java
@@ -0,0 +1,33 @@
+package win.elegentjs.container;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 本类演示Arrays帮助类，提供的方法如下：
+ *
+ *  sort
+ *  parallelSort
+ *  binarySearch
+ *  fill
+ *  copyOf
+ *  asList
+ *  toString
+ *
+ */
+public class ArraysSample {
+
+    public static void main(String[] args) {
+
+        int[] array = new int[1024];
+        Arrays.fill(array, 100);
+
+        System.out.println(Arrays.toString(array));
+
+        Arrays.parallelSort(array);
+
+        int index = Arrays.binarySearch(array, 100);
+        System.out.println("index : " + index);
+
+    }
+}
