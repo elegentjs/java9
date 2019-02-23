6e7b395ac881b6642541d1da2a44cd9b60408014
liupj <MooN_2012@qq.com>
2019-02-23 11:09:38 +0800

    update

---
+++ src/main/java/win/elegentjs/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package win.elegentjs.basic;
+
+import java.util.Arrays;
+
+/**
+ * 最简单java程序，打印hello world
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(Arrays.toString(args));
+    }
+}
