c2eabd5729bc8d8c3db5dea87bce70a5e07a015e
liupj <MooN_2012@qq.com>
2019-02-26 20:36:33 +0800

    update

--- src/main/java/win/elegentjs/container/ArrayListSample.java
+++ src/main/java/win/elegentjs/container/ArrayListSample.java
@@ -1,9 +1,21 @@
 package win.elegentjs.container;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ArrayList是集合家族最常见的类
+ *
+ * 是List接口的实现类
+ * 特性：
+ *      有序可重复
+ *      可随机存取元素
+ *      底层基于数组，所以擅长随机访问，不擅长添加，删除
+ *
+ */
 public class ArrayListSample {
 
-
     public static void main(String[] args) {
-
+        List<Integer> integerList = new ArrayList<>(10);
     }
 }
