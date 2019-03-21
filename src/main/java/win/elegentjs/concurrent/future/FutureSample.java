073a0cb29ccd609c4fc23c9d48f200b430fc25ee
liupj <MooN_2012@qq.com>
2019-03-21 13:07:31 +0800

    update

---
+++ src/main/java/win/elegentjs/concurrent/future/FutureSample.java
@@ -0,0 +1,28 @@
+package win.elegentjs.concurrent.future;
+
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class FutureSample {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FutureTask<String> futureTask = new FutureTask<>(() -> {return "hello";});
+
+        new Thread(futureTask).start();
+
+        System.out.println("doSomething");
+
+        String s = futureTask.get();
+
+        System.out.println("result : " + s);
+
+    }
+}
+
+
+
+
+
+
