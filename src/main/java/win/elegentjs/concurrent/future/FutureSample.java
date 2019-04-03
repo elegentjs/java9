5a6dfae326cb394eb0d51aec73a6ed78ab17f5bb
liupj <MooN_2012@qq.com>
2019-04-03 16:32:14 +0800

    update

--- src/main/java/win/elegentjs/concurrent/future/FutureSample.java
+++ src/main/java/win/elegentjs/concurrent/future/FutureSample.java
@@ -4,6 +4,9 @@ package win.elegentjs.concurrent.future;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+/**
+ * FutureTask也可以实现闭锁的效果：即等待线程执行完成才能通过
+ */
 public class FutureSample {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
