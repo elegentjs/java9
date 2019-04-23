caf4e2ff6e88051bc1d0c58c3d38f95e740054f6
liupj <MooN_2012@qq.com>
2019-04-23 15:57:35 +0800

    update

--- src/main/java/win/elegentjs/nio/FileChannelSample.java
+++ src/main/java/win/elegentjs/nio/FileChannelSample.java
@@ -5,11 +5,14 @@ import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+/**
+ * 使用NIO读取文件并打印出来
+ */
 public class FileChannelSample {
 
     public static void main(String[] args) {
         try (
-                RandomAccessFile randomAccessFile = new RandomAccessFile("/Users/liupeijun/git/00_bash/redeploy.sh", "rw");
+                RandomAccessFile randomAccessFile = new RandomAccessFile("/Users/liupeijun/Downloads/settings.xml", "rw");
                 FileChannel fileChannel = randomAccessFile.getChannel();
         ) {
 
